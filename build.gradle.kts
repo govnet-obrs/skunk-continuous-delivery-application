8e8150d6a37aa8ec4aa1d7af83de13a047b8e34e
Ronald Adonyo <radonyo@theprocompanies.com>
2023-03-11 21:26:10 +0300

    test ci/cd fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,7 +50,7 @@ jib{
         }
     }
     to {
-        image = "zalerix/continuous-delivery-example"
+        image = "gfsekamanya/continuous-delivery-example"
         tags = setOf("latest", "${project.version}")
         auth {
             username = System.getenv("DOCKER_USER")
